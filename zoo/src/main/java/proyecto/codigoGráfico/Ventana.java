f042cf734bb49f271c653776d876e268692a4d00
María Jesús <marolivares2023@udec.cl>
2024-06-20 06:36:27 -0400

    fix: tamaño ventana

--- zoo/src/main/java/proyecto/codigoGráfico/Ventana.java
+++ zoo/src/main/java/proyecto/codigoGráfico/Ventana.java
@@ -6,7 +6,7 @@ public class Ventana extends JFrame {
 
     public Ventana() {
         super();
-        setSize(1000, 1000); //Tamaño de los habitat: 5000x4500 (Adaptado(:5): 1000x900)
+        setSize(1000, 900); //Tamaño de los habitat: 5000x4500 (Adaptado(:5): 1000x900)
         setTitle("Zoológico");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
